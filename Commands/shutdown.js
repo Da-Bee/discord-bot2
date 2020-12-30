7deae584f1e9f74ab549048e762592c2a31d3c0c
aggaran21 <53624272+aggaran21@users.noreply.github.com>
2020-12-30 09:57:47 -0600

    Create shutdown.js

---
+++ Commands/shutdown.js
@@ -0,0 +1,14 @@
+const { ownerid } = require('../config.json');
+
+module.exports = {
+        name: 'shutdown',
+        description: 'Shuts down the bot. BOT OWNER ONLY!!',
+        execute(message) {
+                if (message.author.id === `${ownerid}`) {
+                message.channel.send('Shutting down...')        
+                process.exit();
+                } else if (message.author.id != `${ownerid}`) {
+                        message.channel.send(`Only the bot owner can use this c>
+                }
+                },
+        };
