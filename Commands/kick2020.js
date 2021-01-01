5008a1587bfd65cae376040ff13997a4833beca4
aggaran21 <53624272+aggaran21@users.noreply.github.com>
2021-01-01 00:17:44 -0600

    dumbness

--- Commands/kick2020.js
+++ Commands/kick2020.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'kick2020',
   description: 'Well, its obvious.',
-  message(exports) {
+  execute(message) {
     message.channel.send('https://cdn.discordapp.com/attachments/748934237624729710/794439380345421854/foot_kicking_2020_dumpster_fire_out_of_a_door_insert_by_Bigstock.jpg')
   },
 };
