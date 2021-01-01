e761942c82f614094a5876c3e6db28b798d8d845
aggaran21 <53624272+aggaran21@users.noreply.github.com>
2021-01-01 00:24:12 -0600

    ...idrc abt these anymore

--- Commands/kick2020.js
+++ Commands/kick2020.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'kick2020',
+  aliases: 'fucc2020',
   description: 'Well, its obvious.',
   execute(message) {
     message.channel.send('https://cdn.discordapp.com/attachments/748934237624729710/794439380345421854/foot_kicking_2020_dumpster_fire_out_of_a_door_insert_by_Bigstock.jpg')
