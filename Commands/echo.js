798fe47f025344d255ad53175dd40224e16f9581
aggaan21 <anshagg58@gmail.com>
2021-01-05 10:50:10 -0600

    echo, echo, echo

---
+++ Commands/echo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'echo',
+  aliases: ['repeat'],
+  description: 'repeats your words',
+  usage: '<message>',
+  execute(message, args) {
+    message.channel.send(message.content.slice(5, message.content.length));
+  },
+};
