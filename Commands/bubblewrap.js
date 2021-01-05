110a2856fba0753a7116c1eca09c240d7bd1fef8
aggaan21 <anshagg58@gmail.com>
2021-01-05 11:38:59 -0600

    cooldown

--- Commands/bubblewrap.js
+++ Commands/bubblewrap.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: 'bubblewrap',
   description: 'Pretty self-explanitory...',
+  cooldown: 15,
   execute(message) {
     message.channel.send( '|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||')
   },
