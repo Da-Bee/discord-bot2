67ebbffe3401447b1cfca84c5517aee67354145b
aggaran21 <53624272+aggaran21@users.noreply.github.com>
2021-01-05 10:27:15 -0600

    lots of new changes

---
+++ Commands/bubblewrap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'bubblewrap',
+  description: 'Pretty self-explanitory...',
+  execute(message) {
+    message.channel.send( '|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||\n|| pop |||| pop |||| pop |||| pop |||| pop ||')
+  },
+};
