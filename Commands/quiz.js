a8adfb22c985aa952c68a5daa105a75a4dc6d653
aggaan21 <anshagg58@gmail.com>
2021-01-06 11:39:43 -0600

    limit

--- Commands/quiz.js
+++ Commands/quiz.js
@@ -4,20 +4,24 @@ module.exports = {
   guildOnly: true,
   permissions: 'MANAGE_GUILD',
   execute(message, args) {
-		const quiz = require('./quiz.json');
-		const item = quiz[Math.floor(Math.random() * quiz.length)];
-		const filter = response => {
-			return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-		};
+		if (message.author.id === `${ownerid}`) {
+      const quiz = require('./quiz.json');
+  		const item = quiz[Math.floor(Math.random() * quiz.length)];
+  		const filter = response => {
+  			return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+  		};
 
-		message.channel.send(item.question).then(() => {
-			message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-				.then(collected => {
-					message.channel.send(`${collected.first().author} got the correct answer!`);
-				})
-				.catch(collected => {
-					message.channel.send('Looks like nobody got the answer this time.');
-				});
-		});
+  		message.channel.send(item.question).then(() => {
+  			message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+  				.then(collected => {
+  					message.channel.send(`${collected.first().author} got the correct answer!`);
+  				})
+  				.catch(collected => {
+  					message.channel.send('Looks like nobody got the answer this time.');
+  				});
+  		});
+    } else if (message.author.id !=`${ownerid}`) {
+      message.channel.send('You don\'t have permissionto use this command!')
+    }
   },
 };
--- Commands/scramble.js
+++ Commands/scramble.js
@@ -2,22 +2,25 @@ module.exports = {
   name: 'scramble',
   description: 'Pulls a unscramble for the Unbelivaboat Coins',
   guildOnly: true,
-  permissions: 'MANAGE_GUILD',
   execute(message, args) {
-		const quiz = require('./scramble.json');
-		const item = quiz[Math.floor(Math.random() * quiz.length)];
-		const filter = response => {
-			return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-		};
+    if (message.author.id === `${ownerid}`) {
+      const quiz = require('./scramble.json');
+  		const item = quiz[Math.floor(Math.random() * quiz.length)];
+  		const filter = response => {
+  			return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+  		};
 
-		message.channel.send(item.question).then(() => {
-			message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-				.then(collected => {
-					message.channel.send(`${collected.first().author} got the correct answer!`);
-				})
-				.catch(collected => {
-					message.channel.send('Looks like nobody unscrambled the word this time.');
-				});
-		});
+  		message.channel.send(item.question).then(() => {
+  			message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+  				.then(collected => {
+  					message.channel.send(`${collected.first().author} got the correct answer!`);
+  				})
+  				.catch(collected => {
+  					message.channel.send('Looks like nobody unscrambled the word this time.');
+  				});
+  		});
+    } else if (message.author.id != `${ownerid}`) {
+      message.channel.send('You don\'t have permission to use this command!')
+    }
   },
 };
